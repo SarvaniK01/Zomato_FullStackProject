9a14202d9c494e0496cca68e4053f540a12bebaf
Sarvani Kandukuri <sarvanik60@gmail.com>
2022-01-03 17:10:26 +0530

    Foodtab Updated

--- client/src/Components/FoodTab/index.js
+++ client/src/Components/FoodTab/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 
-import {useParams, Link} from "react-router-dom"
+import {useParams, Link} from "react-router-dom";
 
 import {GiScooter, GiKnifeFork, GiBeerBottle} from "react-icons/gi";
 
